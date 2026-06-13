27302dc234103bacd9dee45c58da87b425a39bff
ananthakumarbCSE <ananthakumarb.cse2024@citchennai.net>
2026-06-13 12:10:17 +0530

    feat: configure Next.js rewrite proxy for api calls

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://lifeos-api-qnfy.onrender.com/api/:path*',
+      },
+    ];
+  },
 };
 
+
 export default nextConfig;
+
